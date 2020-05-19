ad554b7a3f2ea99a043edf004c63c43087e22583
Jamie Eriksson <jamieeriksson@gmail.com>
2020-05-19 15:32:29 -0700

    Install babel and html plugin to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/client/index.js",
+  entry: "./src/client/main.js",
   output: {
-    path: __dirname + "/build",
     filename: "bundle.js",
+    path: path.resolve(__dirname, "/build"),
+  },
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: "/node_modules/",
+        loader: "babel-loader",
+      },
+    ],
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/client/views/index.html",
+        filename: "./index.html",
+      }),
+    ],
   },
 };
