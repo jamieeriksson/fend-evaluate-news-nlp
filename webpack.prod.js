26626dac2097ad5a057de4017291f456d64a407a
Jamie Eriksson <jamieeriksson@gmail.com>
2020-05-21 10:18:21 -0700

    Add CSS minifier and terser plugins to prod mode

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,9 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -9,12 +12,19 @@ module.exports = {
     path: path.resolve(__dirname, "build/"),
     filename: "bundle.js",
   },
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
   module: {
     rules: [
       {
         test: "/.js$/",
         exclude: "/node_modules/",
         loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
@@ -23,5 +33,6 @@ module.exports = {
       template: "./src/client/views/index.html",
       filename: "./index.html",
     }),
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
   ],
 };
