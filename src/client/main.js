7fcd922596d3506328e66029cba1cc83fd6aec18
Jamie Eriksson <jamieeriksson@gmail.com>
2020-05-25 16:46:02 -0700

    Change UI on submit, add article analysis info

--- src/client/main.js
+++ src/client/main.js
@@ -4,14 +4,14 @@ import "./styles/styles.scss";
 function changeUI(data) {
   console.log(data);
   const { url, title } = data.article;
-  const {
-    polarity,
-    subjectivity,
-    polarityConfidence,
-    subjectivityConfidence,
-  } = data.sentiment;
-
-  document.getElementById("polarity");
+  let text = document.createTextNode(title);
+  let elmt = document.getElementById("article-title");
+  elmt.appendChild(text);
+  Object.keys(data.sentiment).forEach((key) => {
+    text = document.createTextNode(data.sentiment[key]);
+    elmt = document.getElementById(`${key}`);
+    elmt.appendChild(text);
+  });
 }
 
 async function analyze(event) {
